1c8cbae20b7ef8e8cd2b86bd6c6c3e538e88c89b
weirdcease <78571139+weirdcease@users.noreply.github.com>
2023-03-24 13:17:40 -0400

    aw man

--- curseframe.py
+++ curseframe.py
@@ -30,23 +30,45 @@ scheme_defaults = {
 }
 
 class Menu:
-    def __init__(self, app: App, *, items: tuple[MenuItem], controls: dict = scheme_defaults["menu_vertical"]):
+    def __init__(self, app: App, *, items: tuple[MenuItem], controls: dict = scheme_defaults["menu_vertical"], remember_state: bool = False):
         self.items = items
         self.selected = -1
         self.controls = controls
+        self.remember = remember_state
+        self.app = app
 
     def render(self, y: int = None, x: int = None):
         for i in range(len(self.items)):
             style = curses.A_STANDOUT if self.selected == i else curses.A_NORMAL
-            self.parent.stdscr.addstr(y + i, x, self.items[i].name, style)
+            self.app.stdscr.addstr(y + i, x, self.items[i].name, style)
 
     def focus(self, y: int = None, x: int = None):
+        if self.selected == -1 or not self.remember:
+            self.selected = 0
+
         while True:
             self.render(y, x)
-            key = self.parent.stdscr.getkey()
+            key = self.app.stdscr.getkey()
             for action in self.controls:
                 if key in self.controls[action]:
                     do_action = action
                     break
 
-            
+            if do_action == "next":
+                if self.selected < len(self.items) - 2:
+                    self.selected += 1
+                else:
+                    self.selected = 0
+
+            elif do_action == "prev":
+                if self.selected > 0:
+                    self.selected -= 1
+                else:
+                    self.selected = len(self.items) - 1
+
+            elif do_action == "select":
+                self.items[self.selected].run()
+
+            elif do_action == "break":
+                self.selected = -1
+                break
